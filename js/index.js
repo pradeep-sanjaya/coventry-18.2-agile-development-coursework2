b9ed99ece1b5aa63c912f79d606c92c2d56f9f03
ebayshashila <ebayshashila@gmail.com>
2020-09-26 17:28:59 +0530

    added press 3 functionality and ui parts

--- js/index.js
+++ js/index.js
@@ -1,4 +1,38 @@
 function pageLoad() {
 
+    var btns = document.querySelectorAll('#calculator span');
+    var inputScreen = document.querySelector('#screen');
+    var btnValue;
+    var input;
+
+    for(var i=0; i< btns.length; i++) {
+
+        var decimalAdded = false; // Flag used to avoid two decimal
+
+        btns[i].addEventListener('click', function () {
+
+            btnValue = this.innerHTML;
+            input = inputScreen.innerHTML;
+
+            switch (btnValue) {
+               
+                default:
+                    inputScreen.innerHTML += btnValue;
+                    decimalAdded = false;
+                    playAudio(btnValue);
+                    break;
+            }
+        });
+    }
    
+}
+
+function playAudio(number) {
+    var audio = null;
+    switch(number) {
+        case '3':
+         audio = new Audio('sounds/three.wav');
+    }
+   
+    audio.play();
 }
